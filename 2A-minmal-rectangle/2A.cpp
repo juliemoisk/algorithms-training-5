4df394ee12eaf923a699dbd5356f63ab9134e564
Julia Sklyarova <167636548+juliemoisk@users.noreply.github.com>
2024-07-29 17:18:38 +0300

    Update 2A.cpp

--- 2A-minmal-rectangle/2A.cpp
+++ 2A-minmal-rectangle/2A.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int k;
+    cin >> k;
+
+    int minx, miny, maxx, maxy;
+    cin >> minx >> miny;
+    maxx = minx;
+    maxy = miny;
+
+    for (int i = 0; i < k - 1; ++i) {
+        int x, y;
+        cin >> x >> y;
+        minx = min(minx, x);
+        maxx = max(maxx, x);
+        miny = min(miny, y);
+        maxy = max(maxy, y);
+    }
+
+    cout << minx << " " << miny << " " << maxx << " " << maxy << endl;
+    return 0;
+}
 
