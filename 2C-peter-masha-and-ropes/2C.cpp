7bd8d50973c8adaac94ad9f248d40f8f6302bd27
Julia Sklyarova <167636548+juliemoisk@users.noreply.github.com>
2024-07-29 17:32:49 +0300

    Update 2C.cpp

--- 2C-peter-masha-and-ropes/2C.cpp
+++ 2C-peter-masha-and-ropes/2C.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <numeric> 
+#include <algorithm> 
+
+int main() {
+    int n;
+    std::cin >> n;
+
+    std::vector<int> a(n);
+    for (int i = 0; i < n; ++i) {
+        std::cin >> a[i];
+    }
+
+    int suma = std::accumulate(a.begin(), a.end(), 0);
+    int maxa = *std::max_element(a.begin(), a.end());
+
+    if (maxa * 2 > suma) {
+        std::cout << (maxa * 2 - suma) << std::endl;
+    }
+    else {
+        std::cout << suma << std::endl;
+    }
+
+    return 0;
+}
 
