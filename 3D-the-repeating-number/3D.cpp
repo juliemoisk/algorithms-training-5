0da27d5c4ef0214b0fd2bf67625a021c8f10b32c
Julia Sklyarova <167636548+juliemoisk@users.noreply.github.com>
2024-07-29 18:54:24 +0300

    Update 3D.cpp

--- 3D-the-repeating-number/3D.cpp
+++ 3D-the-repeating-number/3D.cpp
@@ -1,1 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+
+bool sol(const std::vector<int>& nums, int k) {
+    std::unordered_set<int> lastk;
+    for (int i = 0; i < nums.size(); ++i) {
+        if (lastk.find(nums[i]) != lastk.end()) {
+            return true;
+        }
+        lastk.insert(nums[i]);
+        if (i >= k) {
+            lastk.erase(nums[i - k]);
+        }
+    }
+    return false;
+}
+
+int main() {
+    int n, k;
+    std::cin >> n >> k;
+
+    std::vector<int> nums(n);
+    for (int i = 0; i < n; ++i) {
+        std::cin >> nums[i];
+    }
+
+    if (sol(nums, k)) {
+        std::cout << "YES" << std::endl;
+    } else {
+        std::cout << "NO" << std::endl;
+    }
+
+    return 0;
+}
 
