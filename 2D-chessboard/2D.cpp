307a5d03d303a42445a6a7a86d5157f25bbc7058
Julia Sklyarova <167636548+juliemoisk@users.noreply.github.com>
2024-07-29 17:38:35 +0300

    Update 2D.cpp

--- 2D-chessboard/2D.cpp
+++ 2D-chessboard/2D.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+    int n;
+    std::cin >> n;
+
+    std::vector<std::vector<int>> field(10, std::vector<int>(10, 0));
+
+    for (int k = 0; k < n; ++k) {
+        int i, j;
+        std::cin >> i >> j;
+        field[i][j] = 1;
+    }
+
+    int ans = 0;
+    for (int i = 1; i < 10; ++i) {
+        for (int j = 1; j < 10; ++j) {
+            ans += (field[i][j - 1] != field[i][j]);
+            ans += (field[i - 1][j] != field[i][j]);
+        }
+    }
+
+    std::cout << ans << std::endl;
+    return 0;
+}
 
