703cfb48df364499fe4b94c84bebb51a8e7ceb9b
Julia Sklyarova <167636548+juliemoisk@users.noreply.github.com>
2024-07-29 17:27:13 +0300

    Update 2B.cpp

--- 2B-fish-seller/2B.cpp
+++ 2B-fish-seller/2B.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <algorithm> 
+
+int main() {
+    int n, k;
+    std::cin >> n >> k;
+
+    std::vector<int> a(n);
+    for (int i = 0; i < n; ++i) {
+        std::cin >> a[i];
+    }
+
+    int maxinc = 0;
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j <= k; ++j) {
+            if (i + j < n) {
+                maxinc = std::max(maxinc, a[i + j] - a[i]);
+            }
+        }
+    }
+
+    std::cout << maxinc << std::endl;
+    return 0;
+}
 
